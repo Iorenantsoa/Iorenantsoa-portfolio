57426d195a2973bc0c9ad4a175e8813584f8f2fd
Iorenantsoa <antsamalalacedriciorenantsoa@gmail.com>
2024-08-22 19:49:28 +0000

    fin partie about et debut de la partie service

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -2,11 +2,7 @@ import { Particles } from 'react-tsparticles'
 import { loadFull } from 'tsparticles';
 import React, { useCallback } from 'react';
 
-
-
 const ParticlesContainer = () => {
-
-  // init
 
   const particlesInit = useCallback(async (engine) => {
     await loadFull(engine)
@@ -20,81 +16,79 @@ const ParticlesContainer = () => {
       id='tsparticles'
       init={particlesInit}
       loaded={particlesLoaded}
-      options={
-        {
-          fullScreen: { enable: false },
-          background: {
-            color: {
-              value: ''
-            },
+      options={{
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: ''
           },
-          fps_limit: 120,
-          interactivity: {
-            events: {
-              onclick: {
-                enable: false,
-                mode: 'push'
-              },
-              onhover: {
-                enable: true,
-                mode: 'repulse'
-              },
-              resize: true,
+        },
+        fps_limit: 120,
+        interactivity: {
+          events: {
+            onclick: {
+              enable: true, // Particles will be attracted to the mouse click position
+              mode: 'repulse'
             },
-            modes: {
-              push: {
-                quantity: 90
-              },
-              repulse: {
-                distance: 200,
-                duration: 0.4
-              }
+            onhover: {
+              enable: true,
+              mode: 'repulse' // Particles are attracted to the cursor on hover
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 90
+            },
+            attract: {
+              distance: 200,
+              duration: 0.4
             }
+          }
+        },
+        particles: {
+          color: {
+            value: '#2e8ee6' // Change the color of the particles
           },
-          particles: {
-            color: {
-              value: '#e68e2e'
-            },
-            links: {
-              color: '#f5d393',
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              directions: 'none',
-              enable: true,
-              outModes: {
-                default: 'bounce'
-              },
-              random: false,
-              speed: 1,
-              straight: false
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800
-              },
-              value: 80
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: 'circle'
-            },
-            size: {
-              value: { min: 1, max: 5 }
-            },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1
           },
-          detectRetina : true
-        }
-      }
+          collisions: {
+            enable: true,
+          },
+          move: {
+            directions: 'none',
+            enable: true,
+            outModes: {
+              default: 'bounce'
+            },
+            random: false,
+            speed: 1, // Increase speed
+            straight: false
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800
+            },
+            value: 100
+          },
+          opacity: {
+            value: 0.7,
+          },
+          shape: {
+            type: 'square' // Change shape to square
+          },
+          size: {
+            value: { min: 1, max: 7 } // Increase the size range
+          },
+        },
+        detectRetina: true
+      }}
     />
   )
 };
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 // icons
 import {
   FaHtml5,
@@ -15,9 +16,12 @@ import {
   SiAdobephotoshop,
 } from "react-icons/si";
 
+import { motion } from 'framer-motion'
+import { fadeIn } from '../../variants'
+
 
 //  data
-const aboutData = [
+export const aboutData = [
   {
     title: 'skills',
     info: [
@@ -88,8 +92,142 @@ const aboutData = [
   },
 ];
 
+import Avatar from '../../components/Avatar'
+import Circles from '../../components/Circles'
+import CountUp from 'react-countup';
+
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0)
+  console.log(index)
+
+  return (
+    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+      <Circles />
+      {/* Avatar image */}
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]">
+        <Avatar />
+      </motion.div>
+      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center'>
+          <motion.h2 className='h2'
+            variants={fadeIn('right', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+          >Captivating <span className='text-accent'>stories</span>birth magnificent designs
+
+          </motion.h2>
+          <motion.p
+            variants={fadeIn('right', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='max-w-[500] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0'>
+            10 years ago, Ibegan freelancing as a developer, Since then, I{"'"}ve done remote work for agencies, counsulted for startups, andd collaborated on digitalproducts for busines and consumer use.
+          </motion.p>
+          {/* counter */}
+          <motion.div
+            variants={fadeIn('right', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='hidden md:flex md:max-w-xl xl:max-w-none mx-auto xl:mx-0'>
+            <div className='flex flex-1 xl:gap-x-6'>
+              {/* experience */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={10} duration={5} />+
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Years of experience</div>
+              </div>
+              {/* clients */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={250} duration={5} />+
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Stisfied clients</div>
+              </div>
+              {/* projects */}
+              <div className='relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0'>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={650} duration={5} />+
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Finished projects</div>
+              </div>
+              {/* awards */}
+              <div className='relative flex-1  '>
+                <div className='text-2xl xl:text-4xl font-extrabold text-accent mb-2'>
+                  <CountUp start={0} end={8} duration={5} />+
+                </div>
+                <div className='text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]'>Winning awards</div>
+              </div>
+            </div>
+          </motion.div>
+
+
+        </div>
+
+
+        {/* info */}
+        <motion.div
+          variants={fadeIn('left', 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className='flex flex-col w-full xl:max-w-[48%] h-[480px]'>
+
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {
+              aboutData.map((item, itemIndex) => {
+                return (
+                  <div
+                    key={itemIndex}
+                    className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'}
+                     cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px]
+                      after:bg-white after:absolute after:-bottom-1 after:left-0 z-10`}
+                    onClick={() => setIndex(itemIndex)}
+                  >
+                    {item.title}
+                  </div>
+                )
+              })
+            }
+          </div>
+
+          <div className=' py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
+                >
+                  <div className="font-light mb-2 md:mb-0 ">{item.title}</div>
+                  <div className='hidden md:flex'>-</div>
+                  <div>{item.stage}</div>
+                  <div className='flex gap-x-4'>
+                    {item.icons?.map((icon, itemIndex) => {
+                      return <div className='text-2xl text-white' key={itemIndex}> {icon}</div>
+                    })}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+
+
+
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
